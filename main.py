31e452d9727ff708fb3386f3725126209a7b7fab
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2021-05-06 23:48:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import pywhatkit 
-pywhatkit.sendwhatmsg('+91 9830280164','This is a test', 00, 10)
+pywhatkit.sendwhatmsg('+91 9830280164','This is an automated test', 00, 10)
 pywhatkit.info("Python",lines=3)
